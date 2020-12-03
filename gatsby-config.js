0fe2eb2093ef563ca190dec9a4596f3a2d454ec6
manitu85 <manitu.psy@gmail.com>
2020-12-03 21:52:48 +0100

    #

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+  path: `.env`
 })
 
 module.exports = {
@@ -26,7 +26,7 @@ module.exports = {
           "type": process.env.FIREBASE_TYPE,
           "project_id": process.env.FIREBASE_PROJECT_ID,
           "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-          "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+          "private_key": process.env.FIREBASE_PRIVATE_KEY,
           "client_email": process.env.FIREBASE_CLIENT_EMAIL,
           "client_id": process.env.FIREBASE_CLIENT_ID,
           "auth_uri": process.env.FIREBASE_AUTH_URI,
